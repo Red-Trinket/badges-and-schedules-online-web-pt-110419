fc4adea550bcfbb68802787ba56d55d84126a50c
Matthew Jones <matts3371@gmail.com>
2019-11-11 23:56:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,12 @@ def badge_maker(name)
   "Hello, my name is #{name}"
 end 
 
-def batch_badge_creator(names)
-  
+def batch_badge_creator(array)
+  newArray = []
+  array.each do |name|
+    newArray.push("Hello, my name is #{name}")
+  end
+  newArray
+end
+
+def assign_rooms
