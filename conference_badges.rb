29209c58a172f009b8a1e08d16a2a78ab23b41b7
Matthew Jones <matts3371@gmail.com>
2019-11-12 00:11:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,11 @@
 def badge_maker(name)
-  "Hello, my name is #{name}"
+  "Hello, my name is #{name}."
 end 
 
 def batch_badge_creator(array)
   newArray = []
   array.each do |name|
-    newArray.push("Hello, my name is #{name}")
+    newArray.push("Hello, my name is #{name}.")
   end
   newArray
 end
@@ -14,7 +14,7 @@ def assign_rooms(speakers)
   counter = 0
   roomAssign = []
   speakers.each do |speaker|
-    roomAssign.push("Hello, #{speaker}! You'll be assigned to room #{counter}")
+    roomAssign.push("Hello, #{speaker}! You'll be assigned to room #{counter}.")
     counter += 1 
   end
   return roomAssign
