4938407d6f1b2c97ecc0808581ee64534b6b5e6e
Matthew Jones <matts3371@gmail.com>
2019-11-12 00:06:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,5 +14,14 @@ def assign_rooms(speakers)
   counter = 0
   roomAssign = []
   speakers.each do |speaker|
-    roomAssign.push("Hello, #{speaker}!")
+    roomAssign.push("Hello, #{speaker}! You'll be assigned to room #{counter}")
+    counter += 1 
+  end
+  return roomAssign
+end
+
+def printer 
+  puts batch_badge_creator
+  puts assign_rooms
+end 
     
